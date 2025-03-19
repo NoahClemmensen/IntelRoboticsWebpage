567cf2e308bdac871f4aff0132e6a39c403a3acf
NoahClemmensen <noah0940@edu.sde.dk>
2025-03-19 14:02:09 +0100

    Stuff

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/web/index');
+const usersRouter = require('./routes/api/web/users');
+const robotRouter = require('./routes/api/robots/index');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/web', indexRouter);
+app.use('/api/web/users', usersRouter);
+app.use('/api/robots', robotRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
